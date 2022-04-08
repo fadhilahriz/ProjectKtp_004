ed86aa38c5bcf55332ba523186e4d310ef5318f2
Fadhilah Rizky Setiawan <79432782+fadhilahriz@users.noreply.github.com>
2022-04-08 14:09:42 +0700

    First Commit KTP

---
+++ src/test/java/proj/enterprise/ktp/KtpApplicationTests.java
@@ -0,0 +1,13 @@
+package proj.enterprise.ktp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
